215eb68185cc976bab7c5f8b5ae0c6d945fdc665
Vinicius Calefo Assarice <116103355+viniciuscalefo@users.noreply.github.com>
2025-02-22 20:37:15 -0300

    first commit

---
+++ src/main/java/dev/calefo/CadastroDeNinjas/CadastroDeNinjasApplication.java
@@ -0,0 +1,13 @@
+package dev.calefo.CadastroDeNinjas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeNinjasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeNinjasApplication.class, args);
+	}
+
+}
