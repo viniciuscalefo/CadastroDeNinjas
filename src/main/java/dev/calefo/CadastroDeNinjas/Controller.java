77bd8101d859d64664d1d6ad676f5d3af9835e47
Vinicius Calefo Assarice <116103355+viniciuscalefo@users.noreply.github.com>
2025-02-24 12:29:48 -0300

    Criado a controller

---
+++ src/main/java/dev/calefo/CadastroDeNinjas/Controller.java
@@ -0,0 +1,15 @@
+package dev.calefo.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas(){
+        return "Essa é minha primeira mensagem na API";
+    }
+}
