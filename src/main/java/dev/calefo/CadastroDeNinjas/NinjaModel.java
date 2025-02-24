9f99537d0bd81a13bc77a09bfbf0d5feff60d221
Vinicius Calefo Assarice <116103355+viniciuscalefo@users.noreply.github.com>
2025-02-24 17:51:07 -0300

    Cria uma entidade em banco a partir do NinjaModel [Aula3]

---
+++ src/main/java/dev/calefo/CadastroDeNinjas/NinjaModel.java
@@ -0,0 +1,53 @@
+package dev.calefo.CadastroDeNinjas;
+
+import jakarta.persistence.*;
+
+//Entity tranforma uma classe em uma entidade no BD
+//JPA - Java Persistence API
+//Table pode criar o nome da tabela
+@Entity
+@Table(name = "tb_cadastro")
+public class NinjaModel {
+
+    //@ID
+    @Id
+    //Tática de como o ID será gerado automaticamente
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+
+    public NinjaModel() {
+    }
+
+    public NinjaModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
