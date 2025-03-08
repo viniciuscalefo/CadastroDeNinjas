bc31870460bc398ba3c2cefca892403d6f34a351
Vinicius Calefo Assarice <116103355+viniciuscalefo@users.noreply.github.com>
2025-03-08 15:36:10 -0300

    feat: Inclui rotas de criacao, delecao, listagem e listagem por id do Model de Missoes

--- src/main/java/dev/calefo/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/dev/calefo/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,29 +1,39 @@
 package dev.calefo.CadastroDeNinjas.Missoes;
-
-
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController //Fala que tudo que está abaixo sera um controller
 @RequestMapping("missoes") //Começa a mapear as APIs
 public class MissoesController {
+    private  MissoesService missoesService;
+
+    public MissoesController(MissoesService missoesService) {
+        this.missoesService = missoesService;
+    }
 
     @GetMapping("/listar")
-    public String listarMissao(){
-        return "Missao LISTADAS com sucesso";
+    public List<MissoesModel> listarMissoes(){
+        return missoesService.listarMissoes();
+    }
+
+    @GetMapping("/listar/{id}")
+    public MissoesModel listarMissaoPorId(@PathVariable Long id){
+        return missoesService.listarMissaoPorId(id);
     }
 
     @PostMapping("/criar")
-    public String criarMissao(){
-        return "Missao CRIADA com sucesso";
+    public MissoesModel criarMissao(@RequestBody MissoesModel missao){
+        return missoesService.criarMissao(missao);
+    }
+
+    @DeleteMapping("/deletar/{id}")
+    public void deletarNinjaPorId(@PathVariable Long id){
+        missoesService.deletarMissaoPorId(id);
     }
 
     @PutMapping("/alterar")
     public String alterarMissao(){
         return "Missao ALTERADA com sucesso";
-    }
-
-    @DeleteMapping("/deletar")
-    public String deletarMissao(){
-        return "Missao DELETADA com sucesso";
     }
 }
--- src/main/java/dev/calefo/CadastroDeNinjas/Missoes/MissoesService.java
+++ src/main/java/dev/calefo/CadastroDeNinjas/Missoes/MissoesService.java
@@ -1,4 +1,38 @@
 package dev.calefo.CadastroDeNinjas.Missoes;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class MissoesService {
+
+    private MissoesRepository missoesRepository;
+    public MissoesService(MissoesRepository missoesRepository) {
+        this.missoesRepository = missoesRepository;
+    }
+
+    //Listar todas as missoes
+    public List<MissoesModel> listarMissoes(){
+        return missoesRepository.findAll();
+    }
+
+    //Listas missao por ID
+    public MissoesModel listarMissaoPorId(Long id){
+        Optional<MissoesModel> missaoRetornada = missoesRepository.findById(id);
+        return missaoRetornada.orElse(null);
+    }
+
+    //Criar uma nova missao
+    public MissoesModel criarMissao(MissoesModel missao){
+        return missoesRepository.save(missao);
+    }
+
+    //Deletar Missao
+    public void deletarMissaoPorId(Long id){
+        missoesRepository.deleteById(id);
+    }
+
+    //Modificar uma missao
 }
