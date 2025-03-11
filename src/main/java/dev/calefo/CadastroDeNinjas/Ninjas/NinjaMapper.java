1d3387bfb09e81bf0a90fcebcb1983ece7c32ead
Vinicius Calefo Assarice <116103355+viniciuscalefo@users.noreply.github.com>
2025-03-11 20:07:38 -0300

    chore:Inclui a versao do DTO pela entidade que mapeia manualmente no mapper os caminhos entidade x DTO

--- src/main/java/dev/calefo/CadastroDeNinjas/Ninjas/NinjaMapper.java
+++ src/main/java/dev/calefo/CadastroDeNinjas/Ninjas/NinjaMapper.java
@@ -1,4 +1,33 @@
 package dev.calefo.CadastroDeNinjas.Ninjas;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class NinjaMapper {
+
+    public NinjaModel map(NinjaDTO ninjaDTO) {
+        NinjaModel ninjaModel = new NinjaModel();
+        ninjaModel.setId(ninjaDTO.getId());  // Corrigido: ninjaDTO em vez de NinjaDTO
+        ninjaModel.setNome(ninjaDTO.getNome());
+        ninjaModel.setEmail(ninjaDTO.getEmail());
+        ninjaModel.setIdade(ninjaDTO.getIdade());
+        ninjaModel.setImgUrl(ninjaDTO.getImgUrl());
+        ninjaModel.setRank(ninjaDTO.getRank());
+        ninjaModel.setMissoes(ninjaDTO.getMissoes());
+
+        return ninjaModel;
+    }
+
+    public NinjaDTO map(NinjaModel ninjaModel) {
+        NinjaDTO ninjaDTO = new NinjaDTO();
+        ninjaDTO.setId(ninjaModel.getId());  // Corrigido: ninjaModel em vez de NinjaModel
+        ninjaDTO.setNome(ninjaModel.getNome());
+        ninjaDTO.setEmail(ninjaModel.getEmail());
+        ninjaDTO.setIdade(ninjaModel.getIdade());
+        ninjaDTO.setImgUrl(ninjaModel.getImgUrl());
+        ninjaDTO.setRank(ninjaModel.getRank());
+        ninjaDTO.setMissoes(ninjaModel.getMissoes());
+
+        return ninjaDTO;
+    }
 }
